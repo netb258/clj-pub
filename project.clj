27e9b8343e24f1cb50b121e18fc9bb60eb0659a1
netb258 <netb258@abv.bg>
2025-05-25 01:17:15 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.openjfx/javafx-web "17.0.2"]
                  [org.openjfx/javafx-swing "17.0.2"]
                  [me.raynes/fs "1.4.6"] ;; File tools.
-                 ;; This component has been replaced with JavaFX, as it now longer works beyond Java 8.
+                 ;; This component has been replaced with JavaFX, as it no longer works beyond Java 8.
                  ;; [net.sf.cssbox/swingbox "1.1"] ;; Pure Java HTML rendering component.
                  [seesaw "1.5.0"]] ;; GUI library.
   :main ^:skip-aot epub.core
